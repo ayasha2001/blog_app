14785e954caddab4cd7a442b8e09482dc8805e7b
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-07-09 17:30:36 +0530

    initial header skeleton created

---
+++ src/app/components/header/NavItems.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+// Base Imports
+import React, { useState } from 'react'
+
+// Component Imports
+import Dropdown from '../common/Dropdown'
+
+// Other Imports
+import { NAV_ITEMS } from './config'
+
+const NavItems = () => {
+  const [onMouseToggle, setOnMouseToggle] = useState<any>(null)
+
+  const onMouseEnter = (idx:any) => {
+    setOnMouseToggle(idx)
+  }
+
+  const onMouseLeave = () => {
+    setOnMouseToggle(null)
+  }
+
+  return (
+    <div className="flex col-span-3 justify-center h-full">
+      {NAV_ITEMS?.map((item: any, idx: number) => {
+        return <div key={idx} onMouseEnter={() => onMouseEnter(idx)} onMouseLeave={onMouseLeave}>
+              <span className='flex text-white text-xl px-6 items-center uppercase theme-light-blue hover:cursor-pointer h-full'>{item?.title}{
+                item?.children?.length  > 0 ? '^' : null
+              }</span>
+              {
+                (item?.type === 'dropdown' && onMouseToggle === idx ) ? 
+                  <Dropdown items={item?.children}/> 
+                : null
+              }
+            </div>
+      })}
+    </div>
+  )
+}
+
+export default NavItems
---
+++ src/frameworks/utils/enums.ts
@@ -0,0 +1,6 @@
+export enum DeviceType {
+    UNKNOWN = 0,
+    MOBILE = 1,
+    IPAD = 2,
+    TABLET = 3,
+}
